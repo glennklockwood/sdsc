7afcdb7a8324e185b676eb8a870ab2f75447dc99
Glenn K. Lockwood <glock@sdsc.edu>
2013-05-17 11:51:21 -0700

    Offending jobs are sorted by potential severity (number of cores being used)

--- nfsjobs.pl
+++ nfsjobs.pl
@@ -5,6 +5,7 @@
 
 use strict;
 use warnings;
+use File::Basename;
 BEGIN {
   use lib '/home/glock/perl/lib/perl5/site_perl/5.8.8';
   unshift @INC, '/home/glock/perl/lib/perl5/site_perl';
@@ -33,17 +34,34 @@ my $xml_in = `qstat -fx`;
 my $xml = new XML::Simple;
 my $job_list = $xml->XMLin($xml_in)->{Job};
 
-foreach ( @$job_list )
+my @offenders;
+foreach my $job ( @$job_list )
 {
-    next unless defined($_->{Output_Path});
-    next unless $_->{job_state} eq "R";
-    my $path = $_->{Output_Path};
+    next unless defined($job->{Output_Path});
+    next unless $job->{job_state} eq "R";
+    my $path = $job->{Output_Path};
     $path =~ s/^[^:]*://;
 #   next if $path =~ m{^/oasis};
     next unless $path =~ m{^/home};
 
-    printf( "User %12s running job %d out of %s\n",
-        (split(m/@/, $_->{Job_Owner}))[0], 
-        (split(m/\./, $_->{Job_Id}))[0], 
-        $path );
+    if ( $job->{Resource_List}->{nodes} =~ m/^(\d+):ppn=(\d+)/ )
+    {
+        $job->{ct_nodes} = $1;
+        $job->{ct_ppn} = $2;
+        $job->{ct_ranks} = $1 * $2;
+    }
+    $job->{path} = $path;
+
+    push(@offenders, $job);
+}
+
+# print jobs sorted by severity
+printf( "%12s   %7s   %5s   %s\n", 'user', 'jobid', '#ranks', '$PBS_O_WORKDIR' );
+foreach my $job ( sort { $a->{ct_ranks} <=> $b->{ct_ranks} } @offenders )
+{
+    printf( "%12s   %7d   %5d   %s\n",
+        (split(m/@/, $job->{Job_Owner}))[0], 
+        (split(m/\./, $job->{Job_Id}))[0], 
+        $job->{ct_ranks},
+        dirname($job->{path}) );
 }
