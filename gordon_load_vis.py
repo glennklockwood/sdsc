744b98cf7b7beab1714d09256071441dc46403f4
Glenn K. Lockwood <glock@sdsc.edu>
2014-02-03 23:19:38 -0800

    added support for vsmp nodes, added rack labels, various bug fixes

--- gordon_load_vis.py
+++ gordon_load_vis.py
@@ -4,47 +4,75 @@ import numpy as np
 import matplotlib.pyplot as plt
 import re
 
-def hinton(matrix, rack, ax, vsmp ):
+ppn = 16
 
-    max_load = 16.0
+def hinton(matrix, rack, ax, cpugrid):
+    # how wide/tall do our nodes appear in the plot?
+    size_x = 0.50
+    size_y = 1.00
 
-    for (x,y),w in np.ndenumerate(matrix):
+    # coordinates in rack space
+    rack_x = float(rack%10)
+    rack_y = float(int(rack / 10))
+
+    # coordinates in node space
+    node_x = float(rack_x * (10*size_x))
+    node_y = float(rack_y * (10*size_y))
+
+    # add text label for this rack
+    #plt.text(node_x, node_y+size_y*9, "rack%d" % rack)
+    plt.text(node_x, node_y-0.1, "rack %d" % rack)
+
+    supernodes = list()
+
+    for (y,x),w in np.ndenumerate(matrix):
+
         if w < -1.5:
-            color = 'white'
+            color = 'grey'
         elif w < -0.5:
             color = 'red'
-        elif w > (1.1 * max_load):
+        elif w > (1.1 * float(ppn)):
             color = 'yellow'
         else:
-            color = cm(w/max_load)
+            color = cm((0.50 + w/float(cpugrid[y,x]))/1.25)
 
-        size_x = 0.50
-        size_y = 1.00
+        # coordinates on the actual plot
+        plot_x = x*size_x + node_x
+        plot_y = y*size_y + node_y
 
-        # base rack coordinates
-        xpos = float(rack%10)
-        ypos = float(int(rack / 10))
-
-        # node coordinates
-        xoffset = float(xpos * (9*size_x))
-        yoffset = float(ypos * (9*size_y))
-
-        # add text label
-        plt.text(xoffset,yoffset, "rack%d" % rack)
-        print rack, xpos, ypos, xoffset, yoffset
-
-        rect = plt.Rectangle([x*size_x + xoffset, y*size_y + yoffset], 
-            size_x, 
-            size_y,
+        # if this is the last node in a vsmp supernode, go back and
+        # draw the supernode
+        if cpugrid[y, x] > ppn:
+            my_size_x = size_x * 8
+            my_size_y = size_y * 2
+            print "Got a supernode with load %f in rack %d, ppn=%d, x=%d, y=%d" % (w, rack, cpugrid[y,x], x, y)
+        else:
+            my_size_x = size_x
+            my_size_y = size_y
+        
+        rect = plt.Rectangle([plot_x, plot_y], 
+            my_size_x, 
+            my_size_y,
             facecolor=color,
             edgecolor='black')
 
+        if cpugrid[y, x] > ppn:
+            supernodes.append(rect)
+        else:
+            ax.add_patch(rect)
+
+    # draw supernodes last so they are on top of the compute nodes
+    for rect in supernodes:
+        print "adding a supernode!"
         ax.add_patch(rect)
 
 if __name__ == '__main__':
     ### initialize the plot
     ax = plt.gca()
-    ax.patch.set_facecolor('white')
+#   ax.patch.set_facecolor('white')
+    ax.patch.set_facecolor((1,1,1,0.0))
+    ax.axis('off')
+    
     ax.set_aspect('equal', 'box')
     ax.xaxis.set_major_locator(plt.NullLocator())
     ax.yaxis.set_major_locator(plt.NullLocator())
@@ -54,7 +82,7 @@ if __name__ == '__main__':
         ^(\S+)-(\d+)-(\d+)\s+
         \d+\s+
         \d+\s+
-        \d+\s+
+        (\d+)\s+
         \d+/\d+\s+
         \S+\s+
         \S+\s+
@@ -62,8 +90,10 @@ if __name__ == '__main__':
         (\S+)\s*$""", re.VERBOSE)
     staterex = re.compile("(offline|down)")
 
-    rackload = np.empty([8,8])
-    rackload[:] = -2.0
+    cpugrid = np.empty([8,8])
+    cpugrid[:] = ppn
+    loadgrid = np.empty([8,8])
+    loadgrid[:] = -2.0
     thisrack = -1
     ax = plt.gca()
 
@@ -74,26 +104,28 @@ if __name__ == '__main__':
         nodename = match.group(1)
         rack = int(match.group(2))
         slot = int(match.group(3))
-        load = float(match.group(4))
+        cpus = int(match.group(4))
+        load = float(match.group(5))
 
         if rack != thisrack:
+            print "Switching to rack %d" % rack
             if thisrack >= 0:
-                #print rackload
+                hinton( loadgrid, thisrack, ax, cpugrid )
 
-                hinton( rackload, thisrack, ax, 0 )
-
-            rackload[:] = -2.0
+            loadgrid[:] = -2.0
+            cpugrid[:] = ppn
             thisrack = rack
 
-        if staterex.match(match.group(5)):
+        if staterex.match(match.group(6)):
             load = -1.00
 
-        x = slot % 10 - 1
-        y = int(slot/10) - 1
-        rackload[x, y] = load
-        #print x,y,load
+        y = slot % 10 - 1       # column
+        x = int(slot/10) - 1    # row
+
+        loadgrid[x, y] = load
+        cpugrid[x, y] = cpus
 
     ax.autoscale_view()
     ax.invert_yaxis()
-    plt.show()
-
+#   plt.show()
+    plt.savefig('gordon-load.png', bbox_inches='tight')
