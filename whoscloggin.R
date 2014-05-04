7e952221fbf491ab1e9eaa19ee245b91e9f1d6bd
Glenn K. Lockwood <glock@sdsc.edu>
2014-05-03 20:55:01 -0700

    greatly improved+simplified whoscloggin to account for cases where the cluster is empty

--- whoscloggin.R
+++ whoscloggin.R
@@ -1,54 +1,59 @@
 #!/usr/bin/env Rscript
+#       user  jobs nodes cores hours     SUs   SUs.q
+#   arsamimi     0     0     0     0       0    6080
+#       twei     1     5    80    48    3840   11520
+#   dissanay     2     8   128    48    3072    1152
 
-data <- read.table('whoscloggin.in', header=T)
-data$user <- lapply(data$user, as.character)
+data <- read.table('whoscloggin.in', header=T, stringsAsFactors=FALSE)
 
 gen.plot <- function( sorted.data, metric, output.file ) {
-    ### add in cumulative SUs and percents
-    sorted.data$cumul.q <- cumsum(sorted.data[, metric])
-    sorted.data$pct <- sorted.data[,metric] / sum(sorted.data[,metric])
-    sorted.data$cumul.pct <- cumsum(sorted.data$pct)
+    ### filter out empty records
+    idx <- sorted.data[,metric] != 0
+    sorted.data <- sorted.data[idx,]
 
-    ### final row of the data frame containing the sum of each column
-    sorted.data.sums <- c( 'total', colSums(sorted.data[,2:10]) )
+    ### figure out what data to actually plot
+    ### 1. 9 users + "all others"
+    ### 2. exactly 10 users
+    ### 3. no users
+    n.users <- nrow( sorted.data )
+    
+    if ( n.users > 10 ) {
+        x <- sorted.data[0:9,1]
+        y <- sorted.data[0:9,metric]
 
-    ### plot.data is only the top X people.  X is either 9 OR the top 90% of 
-    ### consumers (whichever is smaller)
-    idx <- sorted.data$cumul.pct < 0.90
-    if ( sum(idx) > 9 ) {
-        len.of.list <- length(idx)
-        if ( len.of.list > 9 ) { 
-          idx <- c( rep(TRUE,9), rep(FALSE,len.of.list - 9) )
-        } else {
-          idx <- rep(TRUE,9)
-        }
-    } else if ( sum(idx) == 0 ) {
-      idx <- sorted.data[,metric] > 0
+        # calculate the sum of the remaining users
+        y.total <- sum(sorted.data[,metric])
+        y.remaining <- y.total - sum(y)
+
+        x <- c( x, 'Other Users' )
+        y <- c( y, y.remaining )
+        label <- paste( x, ', ', round(y/1000), 'k', sep="")
+        color <- rainbow(length(y))
+
+        plot <- data.frame( x, y, label, color, stringsAsFactors=FALSE)
+
+    } else if ( n.users == 0 ) {
+        # there are no users to plot
+        x <- c( 'No Users' )
+        y <- c( 1.0 )
+        label <- c( 'No Users' )
+        color <- c( rgb( 1, 1, 1, 0 ) )
+        plot <- data.frame( x, y, label, color, stringsAsFactors=FALSE)
+    } else {
+        # plot all the users
+        x <- sorted.data[0:9,1]
+        y <- sorted.data[0:9,metric]
+        label <- paste( x, ', ', round(y/1000), 'k', sep="")
+        color <- rainbow(length(y))
+        plot <- data.frame( x, y, label, color, stringsAsFactors=FALSE)
     }
-    plot.data <- sorted.data[idx,]
-    
-    ### the total sum of each column of the data shown
-    plot.sums <- c( 'shown', colSums(plot.data[,2:10]) )
-    
-    ### this is the total sum of each column minus the sum of what we're showing 
-    ### to get the sum of the remaining data not shown
-    plot.remainder <- c( 0.0, as.numeric(sorted.data.sums[2:10])  - as.numeric(plot.sums[2:10]) )
-    
-    ### add that final "remainder" row to the data to be plotted
-    plot.data <- rbind( plot.data, as.numeric(plot.remainder) )
-    plot.data[nrow(plot.data),1] <- "Other users"
-    
-    ### generate labels based on user and their percent contribution
-    #plot.labels <- paste(plot.data$user, ', ', round(plot.data$pct*100), '%', sep="")
-    plot.labels <- paste(plot.data$user, ', ', round(plot.data[,metric]/1000), 'k', sep="")
     
     ### finally generate the plot
     png(output.file, width=640, height=480, bg='transparent')
-    par(new=F, mar=c(0, 0, 0, 0))
-    pie(plot.data[, metric],labels=plot.labels, col=rainbow(nrow(plot.data)))
+    par(new=FALSE, mar=c(0, 0, 0, 0))
+    pie(plot$y, labels=plot$label, col=plot$color)
     dev.off()
 }
-
 
 ### sorted dataframe
 sorted.data <- data[with(data, order(-SUs.q)), ]
